195e8508995b398cca259b7512874157da002457
Arjun Balaji <arjunblj@gmail.com>
2015-01-12 19:01:03 -0500

    Set up basic Goodreads OAuth flow.

--- bookviz/urls.py
+++ bookviz/urls.py
@@ -5,11 +5,23 @@ from rauth.service import OAuth1Service
 from bookviz import app
 
 
+base_url='http://www.goodreads.com/oauth/'
+
+goodreads = OAuth1Service(
+    name='goodreads',
+    consumer_key=app.config['GOODREADS_CONSUMER_KEY'],
+    consumer_secret=app.config['GOODREADS_CONSUMER_SECRET'],
+    request_token_url=base_url+'request_token',
+    authorize_url=base_url+'authorize',
+    access_token_url=base_url+'access_token',
+    )
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
 
+    # head_auth=True is important here; this doesn't work with oauth2 for some reason
+    request_token, request_token_secret = goodreads.get_request_token(header_auth=True)
+    authorize_url = goodreads.get_authorize_url(request_token)
 
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome')
+    return render_template('index.html', authorize_url=authorize_url)
